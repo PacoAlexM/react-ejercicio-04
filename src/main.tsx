cc939b79d07bbac7d25634186cdea48191480b5e
PacoAlexM <pako_ale_martell@hotmail.com>
2025-12-29 23:24:37 -0600

    Uso de useRef y uso de apis con hooks intercomunicados

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
-import { TrafficLightWithHooks } from './02-useEffect/TrafficLightWithHooks'
+// import { TrafficLightWithHooks } from './02-useEffect/TrafficLightWithHooks'
+// import { PokemonPage } from './03-examples/PokemonPage'
+import { FocusScreen } from './04-useRef/FocusScreen'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +13,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
-    <TrafficLightWithHooks />
+    {/* <TrafficLightWithHooks /> */}
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
 )
